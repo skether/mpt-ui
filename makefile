d8f04fde67569edc9802cbce6ebef8bd123eaf1f
KissBenji <37180748+KissBenji@users.noreply.github.com>
2018-03-08 15:50:00 +0100

    Decided on ncurses
    Tested Slang and ncurses

---
+++ makefile
@@ -0,0 +1,8 @@
+CC = gcc
+FLAGS = -Wall -std=c99
+
+all: mpt-ui.c
+	$(CC) $(FLAGS) -o mpt-ui mpt-ui.c -lcurses
+
+clean:
+	rm -f *.o mpt-ui
--- mpt-ui.c
+++ mpt-ui.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	initscr(); //Initialize screen
+	cbreak(); //Disable input buffering
+	noecho(); //Dont write input to terminal
+	keypad(stdscr, TRUE); //Get all special characters inut
+
+	//move(x, y) //Move the cursor
+	wrefresh(stdscr);
+
+	int curX = 0;
+	int curY = 0;
+	int ch = 0;
+	while((ch = getch()) != KEY_F(10))
+	{
+		switch(ch)
+		{
+			case KEY_DOWN: curY++; move(curY, curX); break;
+			case KEY_UP: curY--; move(curY, curX); break;
+			case KEY_LEFT: curX--; move(curY, curX); break;
+			case KEY_RIGHT: curX++; move(curY, curX); break;
+			default: break;
+		}
+		wrefresh(stdscr);
+	}
+
+	endwin(); //End the window before exiting
 	return 0;
 }
