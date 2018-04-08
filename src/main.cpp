0d4fd0bdd2a30a1de4e17f460db3f6e67b98a149
Ney <harangi.lajos.1996@gmail.com>
2018-04-08 03:43:23 +0200

    Added comments and cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,18 @@
 
 #include "color.h"
 
-#define nl '\n'
-
-//IMPLEMENTING WINDOWS
+//Base class for window objects
 class Window
 {
 public:
-	int height;
-	int width;
-	int posRow;
-	int posCol;
-	int posDepth;
-	std::vector<int> content;
+	int height;												//Height of the window in rows
+	int width;												//Width of the window in rows
+	int posRow;												//Sets which row the window's upper left corner is.
+	int posCol;												//Sets which column the window's upper left corner is.
+	int posDepth;											//Sets the z-depth of the window
+	std::vector<int> contentBuffer;							//Stores the character information of the window
 
+	//Base Constructor for the object
 	Window(int argHeight, int argWidth, int argPosRow, int argPosCol, int argPosDepth)
 	{
 		height = argHeight;
@@ -28,43 +27,46 @@ public:
 		posCol = argPosCol;
 		posDepth = argPosDepth;
 
-		content.reserve(height * width);
-		content.resize(height * width, 32);
+		contentBuffer.reserve(height * width);
+		contentBuffer.resize(height * width, 32);
 	}
 
-	~Window(){}
-
-	void setCharacter(int row, int col, int newChar)
-	{
-		content[row*width+col] = newChar;
+	//Destructor for the object
+	~Window(){
+		//TODO: Check out how to destruct objects
 	}
 
-	int getCharacter(int row, int col)
-	{
-		return content[row*width+col];
-	}
+	//Sets character at the desired position
+	//This immediately sets the contentBuffer to the new value
+	void setCharacter(int row, int col, int newChar) { contentBuffer[row * width + col] = newChar; }
 
+	//Gets character at the desired position
+	int getCharacter(int row, int col) { return contentBuffer[row * width + col]; }
+
+	//Adds border to the window
+	//This immediately sets the contentBuffer to the new values
 	void setBorder()
 	{
-		for (int i = 1; i < height-1; ++i)
+		for (int i = 1; i < height-1; ++i)					//Setting Vertical borders
 		{
 			setCharacter(i, 0, ACS_VLINE);
 			setCharacter(i, width-1, ACS_VLINE);
 		}
 
-		for (int i = 1; i < width-1; ++i)
+		for (int i = 1; i < width-1; ++i)					//Setting Horizontal borders
 		{
 			setCharacter(0, i, ACS_HLINE);
 			setCharacter(height-1, i, ACS_HLINE);
 		}
 
-		setCharacter(0, 0, ACS_ULCORNER);
-		setCharacter(0, width-1, ACS_URCORNER);
-		setCharacter(height-1, 0, ACS_LLCORNER);
-		setCharacter(height-1, width-1, ACS_LRCORNER);
+		setCharacter(0, 0, ACS_ULCORNER);					//Setting upper left corner
+		setCharacter(0, width-1, ACS_URCORNER);				//Setting upper right corner
+		setCharacter(height-1, 0, ACS_LLCORNER);			//Setting lower left corner
+		setCharacter(height-1, width-1, ACS_LRCORNER);		//Setting lower right corner
 	}
 };
 
+//Temporary method for printing windows (doesn't respect z-depth)
 void printWindow(Window win)
 {
 	erase();
@@ -79,15 +81,20 @@ void printWindow(Window win)
 	refresh();
 }
 
-//MAIN CLASS
+//The programs main entry point
 int main()
 {
-	//Init ncurses
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, true);
+	//**********************//
+	//* Initialize ncurses *//
+	//**********************//
 
+	initscr();												//Initializing ncurses screen
+	cbreak();												//Disable buffering of typed characters
+	noecho();												//Disable automatic echoing of typed characters
+	keypad(stdscr, true);									//Enable capturing of special keys
+
+	//Check if terminal has color support, if not display error the exit the program.
+	//TODO: Right now this projects relies on color support. Black and White only support could be added later.
 	if (has_colors() == FALSE)
 	{
 	    endwin();
@@ -95,8 +102,14 @@ int main()
 	    std::exit(1);
 	}
 
-	start_color();
-	setupColorPairs();
+	start_color();											//Initializing ncurses colors
+	setupColorPairs();										//Setting up the color pairs. Implemented in color.c
+
+
+
+	//**************//
+	//* DEBUG CODE *//
+	//**************//
 
 	wattron(stdscr, COLOR_PAIR(P_BGW));
 	move(0, 0);
@@ -118,9 +131,14 @@ int main()
 	testWindow.setBorder();
 	printWindow(testWindow);
 
-	//MAIN LOOP
-	int ch = 0;
-	while((ch = getch()) != KEY_F(10))
+
+
+	//*********************//
+	//* Main control loop *//
+	//*********************//
+	
+	int ch = 0;												//Stores typed character
+	while((ch = getch()) != KEY_F(10))						//Main control loop. Exit character = F10
 	{
 		switch(ch)
 		{
@@ -129,26 +147,12 @@ int main()
 		}
 	}
 
+
+
+	//************//
+	//* Shutdown *//
+	//************//
+
 	endwin();
-
-	//DEBUG INFO
-	/*std::cout << "testWindow.content[i]\r\n";
-	for (int i = 0; i < testWindow.height*testWindow.width; ++i)
-	{
-		if(testWindow.content[i] != 32)
-		std::cout << i << ":\t" << testWindow.content[i] << "\r\n";
-	}
-
-	std::cout << "\r\ntestWindow.getCharacter(i, j)\r\n";
-	for (int i = 0; i < testWindow.height; ++i)
-	{
-		for (int j = 0; j < testWindow.width; ++j)
-		{
-			if(testWindow.getCharacter(i, j) != 32)
-				std::cout << i << ", " << j << ":\t" << testWindow.getCharacter(i, j) << "\r\n";
-		}
-		
-	}*/
-
 	std::exit(0);
 }
