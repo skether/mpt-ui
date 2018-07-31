2c88f6bee7864cce9ffe8a7d2a12f507ad871582
SketheR <harangi.lajos.1996@gmail.com>
2018-08-01 00:38:46 +0200

    Added Dimension class

---
+++ src/dimension.cpp
@@ -0,0 +1,9 @@
+#include "dimension.h"
+
+Dimension::Dimension(int argWidth, int argHeight, int argX, int argY)
+{
+	width = argWidth;
+	height = argHeight;
+	x = argX;
+	y = argY;
+}
---
+++ src/dimension.h
@@ -0,0 +1,15 @@
+#ifndef DIMENSION_H
+#define DIMENSION_H
+
+class Dimension
+{
+public:
+	int height;
+	int width;
+	int x;
+	int y;
+
+	Dimension(int argWidth, int argHeight, int argX, int argY);
+};
+
+#endif
